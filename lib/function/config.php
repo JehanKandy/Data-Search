4cf11a0eedddedda7ea48423c2ef030fd8bf10c2
JehanKandy <jehankandy@gmail.com>
2023-01-18 07:56:53 +0530

    Add files via upload

---
+++ lib/function/config.php
@@ -0,0 +1,13 @@
+<?php 
+    function Connection(){
+        $server = "localhost";
+        $user = "root";
+        $pass = "";
+        $db_name = "test_data_tbl";
+
+        $con = mysqli_connect($server, $user, $pass, $db_name);
+
+        $result = (!$con)? "Connection Lost" : $con;
+        echo $result;
+    }
+?>
