d417069d91c282fd4844834cc8aca333d1168e2d
JehanKandy <jehankandy@gmail.com>
2023-01-18 07:36:18 +0530

    Add files via upload

---
+++ lib/layouts/nav.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="../../css/style.css">
+<nav>
+    <div class="nav-content">
+        <div class="container">
+            
+        </div>
+    </div>
+</nav>
+
+<script src="../../js/script.js"></script>
