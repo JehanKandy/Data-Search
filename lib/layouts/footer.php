cc7b1341307001c15b81ea0dbbcd7d9f5f5e3321
JehanKandy <jehankandy@gmail.com>
2023-01-18 07:24:26 +0530

    Update footer.php

--- lib/layouts/footer.php
+++ lib/layouts/footer.php
@@ -1,4 +1,12 @@
 <link rel="stylesheet" href="../../css/style.css">
 
+<footer>
+    <div class="footer-content">
+        <div class="container">
+            hi
+        </div>
+    </div>
+</footer>
+
 
 <script src="../../js/script.js"></script>
