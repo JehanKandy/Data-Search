b4ed13094d2e6021a09948181ec8786dc41a800e
JehanKandy <jehankandy@gmail.com>
2023-01-18 13:14:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
                 <form action="<?php $_SERVER['PHP_SELF']?>" method="POST" name="search_form" onsubmit="return validate_search();">
                     <div class="search-form">
                         <input type="text" class="form-input" id="searchValue" placeholder="Search">
+                        <p id="inputerror"></p>
                         <input type="submit" value="Search" class="btn btn-primary search-btn" name="searchBtn">
                     </div>
                 </form>
